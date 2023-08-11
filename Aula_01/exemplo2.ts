0715379ca7e620c2a46793dcddc8bd8bafad4448
SAMUEL SANTOS SOUZA <1091392313014@fatec-srv.local>
2023-08-11 20:33:25 -0300

    Aula_01

---
+++ Aula_01/exemplo2.ts
@@ -0,0 +1,18 @@
+namespace exemplo2 { 
+    //VAR, LET CONST
+    let numero: number;
+    numero = 5;
+
+    let resultado: number;
+    resultado = numero + numero;
+    console.log("A soma entre " + numero + "Com " + numero + "é igual a: " + resultado );
+
+    resultado = numero - 4;
+    console.log(`A subtração entre ${numero} com 4 é igual a: ${resultado}`);
+
+    resultado = numero * numero;
+    console.log(`A multiplicação entre ${numero} com ${numero} é igual a: ${resultado}`);
+
+    resultado = numero / numero;
+    console.log(`A divisão entre ${numero} e ${numero} é igual a: ${resultado}`);
+}
